bfb9bc0a3ff667b5dcbb0cfaf536a615e1edc2c5
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-03-24 21:09:53 +0900

    s

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,53 @@
 class GroupsController < ApplicationController
 
   def index
-    # for num in 1..768 do
-    #   @fields = Field.new
-    #   @fields.id = num
-    #   @fields.char_id = nil
-    #   @fields.team_id = nil
-    #   @fields.save
-    # end
+    for num in 1..768 do
+      @fields = Field.new
+      @fields.id = num
+      @fields.char_id = nil
+      @fields.team_id = nil
+      @fields.save
+    end
+
+    @elementFire = Element.new
+    @elementFire.id = 1
+    @elementFire.name = "火"
+    @elementFire.save
+
+    @elementWater = Element.new
+    @elementWater.id = 2
+    @elementWater.name = "水"
+    @elementWater.save
+
+    @elementWood = Element.new
+    @elementWood.id = 3
+    @elementWood.name = "木"
+    @elementWood.save
+
+    @elementLightness = Element.new
+    @elementLightness.id = 4
+    @elementLightness.name = "光"
+    @elementLightness.save
+
+    @elementDarkness = Element.new
+    @elementDarkness.id = 5
+    @elementDarkness.name = "闇"
+    @elementDarkness.save
+
+    @move1 = Movement.new
+    @move1.id = 1
+    @move1.name = "直線往復"
+    @move1.save
+
+    @move2 = Movement.new
+    @move2.id = 2
+    @move2.name = "追尾"
+    @move2.save
+
+    @move3 = Movement.new
+    @move3.id = 3
+    @move3.name = "散策"
+    @move3.save
   end
 
   def new
