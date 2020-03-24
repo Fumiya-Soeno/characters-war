e62a963b9b81cef4093f6c0ec1b96ae151d59e2e
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-03-24 17:10:07 +0900

    s

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,65 +1,74 @@
-$(function(){ 
+$(function(){
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="main-chat__main__message">
-         <div class="main-chat__main__message__upper">
-           <div class="main-chat__main__message__upper">
-             ${message.user_name}
-           </div>
-           <div class="main-chat__main__message__upper__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="main-chat__main__message__lower">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img class="lower-message__image" src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-     `<div class="main-chat__main__message">
-        <div class="main-chat__main__message__upper">
-          <div class="main-chat__main__message__upper">
-            ${message.user_name}
-          </div>
-          <div class="main-chat__main__message__upper__date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="main-chat__main__message__lower">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>`
-     return html;
-   };
+  image = (message.image) ? `<img class= "main-chat__message-list__message__main__image" src=${message.image} >` : "";
+  var html =
+  `<div class="main-chat__main__message data-message-id=${message.id}">
+    <div class="main-chat__main__message__upper">
+      <div class="main-chat__main__message__upper">
+        ${message.user_name}
+      </div>
+      <div class="main-chat__main__message__upper__date">
+        ${message.created_at}
+      </div>
+    </div>
+    <div class="main-chat__main__message__lower">
+      <p class="lower-message__content">
+        ${message.content}
+      </p>
+    </div>
+    ${image}
+  </div>`
+    return html;
   }
-  $('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.main-chat__main').append(html);
-      $('form')[0].reset();
-      $(".submit").prop('disabled', false);
+  var reloadMessages = function() {
+    var last_message_id = $(".main-chat__main__message:last").data("message-id");
+    if (last_message_id === undefined){
+      last_message_id = 0;
+    }
+    $.ajax({
+      type: 'GET',
+      url: 'api/messages',
+      dataType: 'json',
+      data: { id: last_message_id }
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML += buildHTML(message);
+          $('.main-chat__main').append(insertHTML);
+        })
+      }
       $('.main-chat__main').animate({ scrollTop: $('.main-chat__main')[0].scrollHeight});
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
+      alert('error');
     });
-  })
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "post",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-chat__main').append(html);
+      $('.main-chat__main').animate({ scrollTop: $('.main-chat__main')[0].scrollHeight});
+      $('.submit').prop('disabled', false);
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert('error');
+    })
+  });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 3000);
+  }
 });
